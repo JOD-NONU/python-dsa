3854905ddceb83dd16e46657111b7755db00d062
JOD-NONU <tanushbansal0033@gmail.com>
2026-07-21 00:50:44 +0530

    Completed Hashing

---
+++ 03-Hashing/practice.py
@@ -0,0 +1,42 @@
+#1 Frequency Count
+word = "banana"
+d = {}
+for ch in word:
+    if ch not in d:
+        d[ch]=1
+    else:
+        d[ch]+=1
+print(d)
+
+#2 Duplicate Detection
+nums=[1,2,3,2]
+s=set()
+for i in nums:
+    if i in s:
+        print(True)
+        break
+    s.add(i)
+
+#3 Character Mapping
+s="paper"
+t="title"
+d1={}
+d2={}
+
+#4 Complement Search
+nums=[2,7,11,15]
+target=9
+d={}
+
+#5 Grouping
+eat
+
+↓
+
+aet
+
+tea
+
+↓
+
+aet
